a2841afb56e0b70574dde95d8414262cfeca3ddb
DESKTOP-L48KHIQ\Lenovo <826518555@qq.com>
2020-12-01 09:38:39 +0800

    dubbo-nacos测试完成

--- springboot-dubbo-demo/provider/src/main/java/com/dubbo/impl/UserServiceImpl.java
+++ springboot-dubbo-demo/provider/src/main/java/com/dubbo/impl/UserServiceImpl.java
@@ -2,12 +2,16 @@ package com.dubbo.impl;
 
 import com.dubbo.UserService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Value("${spring.application.name}")
+    private String serviceName;
+
     @Override
     public String SayHello(String name) {
-        return "Hello " + name + "!";
+        return String.format("[%s] : Hello , %s", serviceName, name);
     }
 }
