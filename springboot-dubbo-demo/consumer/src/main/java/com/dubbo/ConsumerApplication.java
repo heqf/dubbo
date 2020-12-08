cabb094e41f89f83cc357d3fe92b0b1fd21d9edc
DESKTOP-L48KHIQ\Lenovo <826518555@qq.com>
2020-12-08 17:33:59 +0800

    测试

--- springboot-dubbo-demo/consumer/src/main/java/com/dubbo/ConsumerApplication.java
+++ springboot-dubbo-demo/consumer/src/main/java/com/dubbo/ConsumerApplication.java
@@ -1,11 +1,13 @@
 package com.dubbo;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDubbo
 @SpringBootApplication
+@NacosPropertySource(dataId = "test.properties", autoRefreshed = true)
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
--- springboot-dubbo-demo/consumer/src/main/java/com/dubbo/controller/UserController.java
+++ springboot-dubbo-demo/consumer/src/main/java/com/dubbo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.dubbo.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.dubbo.UserService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +12,41 @@ public class UserController {
     @Reference  //注入要调用的服务
     private UserService userService;
 
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @NacosValue(value = "${useConfig:测试者age12}", autoRefreshed = true)
+    private String useConfig;
+
+    @NacosValue(value = "${name:测试者}", autoRefreshed = true)
+    private String useName;
+
     @RequestMapping("/user/{name}")
     public String getUser(@PathVariable String name){
         //调用服务
         return userService.SayHello(name);
     }
+
+    @RequestMapping("/getUseLocalCache")
+    @ResponseBody
+    public boolean getUseLocalCache(){
+        //调用服务
+        return useLocalCache;
+    }
+
+    @RequestMapping("/getUseConfig")
+    @ResponseBody
+    public String getUseConfig(){
+        //调用服务
+        return useConfig;
+    }
+
+    @RequestMapping("/getUseName")
+    @ResponseBody
+    public String getUseName(){
+        //调用服务
+        return useName;
+    }
+
+
 }
